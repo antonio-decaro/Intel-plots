ae6609fde1d886ac5d37eb3a048faba98cf7d95a
antonio-decaro <antonio.decaro99@outlook.it>
2023-06-20 18:04:55 +0200

    inserted memory bandwidth graph

--- memory/plot.py
+++ memory/plot.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import hmean
 
 # Read the CSV data for GPU 1 and GPU 2
 gpu1_data = pd.read_csv('V100.csv', delimiter=',')
@@ -10,6 +11,9 @@ gpu2_data = pd.read_csv('ARC_A770.csv', delimiter=',')
 global_size_column = 'Global Size'
 local_size_column = 'Local Size'
 bandwidth_column = 'Memory Bandwidth'
+
+gpu1_mean_data = gpu1_data.groupby([local_size_column, global_size_column])[bandwidth_column].apply(lambda x: hmean(x)).reset_index()
+gpu2_mean_data = gpu2_data.groupby([local_size_column, global_size_column])[bandwidth_column].apply(lambda x: hmean(x)).reset_index()
 
 # Extract the relevant columns
 global_sizes_gpu1 = gpu1_data[global_size_column]
@@ -21,8 +25,8 @@ local_sizes_gpu2 = gpu2_data[local_size_column]
 gpu2_bandwidth = gpu2_data[bandwidth_column]
 
 # Group the data by local sizes
-gpu1_grouped = gpu1_data.groupby(local_size_column)
-gpu2_grouped = gpu2_data.groupby(local_size_column)
+gpu1_grouped = gpu1_mean_data.groupby(local_size_column)
+gpu2_grouped = gpu2_mean_data.groupby(local_size_column)
 
 # Create a 2D plot for each local size group
 fig, axs = plt.subplots(len(gpu1_grouped), sharex=True, sharey=True, figsize=(8, 6 * len(gpu1_grouped)))
@@ -33,14 +37,14 @@ for i, ((local_size_gpu1, group_gpu1), (local_size_gpu2, group_gpu2)) in enumera
     group_gpu2_sorted = group_gpu2.sort_values(by=global_size_column)
 
     # Scatter plot for GPU 1 bandwidth (optional)
-    axs[i].scatter(group_gpu1_sorted[global_size_column], group_gpu1_sorted[bandwidth_column], c='b', label='GPU 1')
+    axs[i].scatter(group_gpu1_sorted[global_size_column], group_gpu1_sorted[bandwidth_column], c='g', label='V100')
     # Scatter plot for GPU 2 bandwidth (optional)
-    axs[i].scatter(group_gpu2_sorted[global_size_column], group_gpu2_sorted[bandwidth_column], c='r', label='GPU 2')
+    axs[i].scatter(group_gpu2_sorted[global_size_column], group_gpu2_sorted[bandwidth_column], c='b', label='ARC A770')
 
     # Plot a line connecting the data points for GPU 1 bandwidth
-    axs[i].plot(group_gpu1_sorted[global_size_column], group_gpu1_sorted[bandwidth_column], c='b', linestyle='-', linewidth=1)
+    axs[i].plot(group_gpu1_sorted[global_size_column], group_gpu1_sorted[bandwidth_column], c='g', linestyle='-', linewidth=1)
     # Plot a line connecting the data points for GPU 2 bandwidth
-    axs[i].plot(group_gpu2_sorted[global_size_column], group_gpu2_sorted[bandwidth_column], c='r', linestyle='-', linewidth=1)
+    axs[i].plot(group_gpu2_sorted[global_size_column], group_gpu2_sorted[bandwidth_column], c='b', linestyle='-', linewidth=1)
 
     axs[i].set_title(f'Local Size: {local_size_gpu1}')
     axs[i].set_xlabel('Global Size')
@@ -48,11 +52,19 @@ for i, ((local_size_gpu1, group_gpu1), (local_size_gpu2, group_gpu2)) in enumera
 
     axs[i].legend()
 
+    axs[i].set_xscale('log')
+
+    # Find the maximum available global size for GPU 2
+    max_global_size_gpu2 = group_gpu2_sorted[global_size_column].max()
+
+    # Add a vertical line to indicate the unavailable data beyond max_global_size_gpu2
+    axs[i].axvline(x=max_global_size_gpu2, color='r', linestyle='--')
+
 # Adjust the spacing between subplots
 plt.tight_layout()
 
 # Save the plot as an image file
-plt.savefig('bandwidth_plot.png', dpi=300)  # Adjust the filename and DPI as needed
+plt.savefig('bandwidth_plot.png', dpi=500)  # Adjust the filename and DPI as needed
 
 # Show the plot (optional)
 plt.show()
--- memory/run_test.py
+++ memory/run_test.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 NUM_GLOBALS = 18
+NUM_ITERS = 20
 
 def double(n):
     num = 1024
@@ -16,7 +17,8 @@ local_sizes = [32, 64, 128, 256, 512, 1024]
 if __name__ == '__main__':
     for glob, loc in ((g, l) for l in local_sizes for g in global_sizes):
         print(glob, loc)
-        out = subprocess.run(['./mem_test.out', str(glob), str(loc)], capture_output=True)
-        if not out.returncode:
-            with open('out.log', "a") as f:
-                f.write(out.stdout.decode('utf-8')) 
+        for i in range(NUM_ITERS):
+            out = subprocess.run(['./mem_test.out', str(glob), str(loc)], capture_output=True)
+            if not out.returncode:
+                with open('out.log', "a") as f:
+                    f.write(out.stdout.decode('utf-8')) 
