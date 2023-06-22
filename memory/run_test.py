93b733c14cc21bf00a45cc2794784f4f2ea24280
antonio-decaro <antonio.decaro99@outlook.it>
2023-06-22 13:06:58 +0200

    modified run_test.py

--- memory/run_test.py
+++ memory/run_test.py
@@ -4,6 +4,8 @@ import subprocess
 NUM_GLOBALS = 18
 NUM_ITERS = 20
 
+FNAME = 'out.log'
+
 def double(n):
     num = 1024
     for i in range(n):
@@ -13,6 +15,8 @@ def double(n):
 global_sizes = [n for n in double(NUM_GLOBALS)]
 local_sizes = [32]#, 64, 128, 256, 512, 1024]
 
+if os.path.exists(FNAME):
+    os.remove(FNAME)
 
 if __name__ == '__main__':
     for glob, loc in ((g, l) for l in local_sizes for g in global_sizes):
@@ -20,5 +24,5 @@ if __name__ == '__main__':
         for i in range(NUM_ITERS):
             out = subprocess.run(['./mem_test.out', str(glob), str(loc)], capture_output=True)
             if not out.returncode:
-                with open('out.log', "a") as f:
+                with open(FNAME, "a") as f:
                     f.write(out.stdout.decode('utf-8')) 
