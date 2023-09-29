8c7afecf03d88425d6b55f37ecd71cc20c6f0c22
antonio-decaro <antonio.decaro99@outlook.it>
2023-09-29 21:36:04 +0200

    updated local memory chart

--- scripts/plots/local_mem.py
+++ scripts/plots/local_mem.py
@@ -1,4 +1,4 @@
-from plot_utils import check_double_input
+from plot_utils import check_double_input, get_double_gpu_name
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -6,22 +6,43 @@ from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import hmean
+import seaborn as sns  # Import seaborn for color palettes
 
 data_gpu1, data_gpu2, out_file = check_double_input()
+name1, name2 = get_double_gpu_name()
 
-mean_gpu1 = data_gpu1.groupby("problem-size")["run-time-throughput"].apply(lambda x: hmean(x))
-data_gpu1 = data_gpu1[data_gpu1["bench-name"] == 'MicroBench_LocalMem_fp32_4096']
-mean_gpu2 = data_gpu2.groupby("problem-size")["run-time-throughput"].apply(lambda x: hmean(x))
-data_gpu2 = data_gpu2[data_gpu2["bench-name"] == 'MicroBench_LocalMem_fp32_4096']
+# Create a dictionary to map GPU names to colors
+gpu_palette = {name1: sns.color_palette("Blues"), name2: sns.color_palette("Reds")}
 
-plt.plot(mean_gpu1.index, mean_gpu1.values, label="MAX 1100")
-plt.plot(mean_gpu2.index, mean_gpu2.values, label="V100")
+# Group the data by 'problem-size' and 'local-size'
+grouped_gpu1 = data_gpu1.groupby(["problem-size", "local-size"])["run-time-throughput"].apply(lambda x: hmean(x))
+grouped_gpu2 = data_gpu2.groupby(["problem-size", "local-size"])["run-time-throughput"].apply(lambda x: hmean(x))
+
+# Get unique 'problem-size' values for x-axis and sort them
+problem_sizes = sorted(data_gpu1["problem-size"].unique())
+
+# Plot a separate line for each 'local-size'
+for idx, local_size in enumerate(data_gpu1["local-size"].unique()):
+    # Get the color for each GPU, cycling through the palette
+    color1 = gpu_palette[name1][idx % len(gpu_palette[name1])]
+    color2 = gpu_palette[name2][idx % len(gpu_palette[name2])]
+    
+    # Filter data for the current 'local-size'
+    data_gpu1_local = grouped_gpu1.xs(local_size, level="local-size")
+    data_gpu2_local = grouped_gpu2.xs(local_size, level="local-size")
+    
+    # Sort the data by 'problem-size'
+    data_gpu1_local = data_gpu1_local.reindex(problem_sizes)
+    data_gpu2_local = data_gpu2_local.reindex(problem_sizes)
+    
+    plt.plot(problem_sizes, data_gpu1_local.values, label=f"{name1} - Local Size {local_size}", color=color1)
+    plt.plot(problem_sizes, data_gpu2_local.values, label=f"{name2} - Local Size {local_size}", color=color2)
 
 # Aggiungi etichette e titolo al grafico
 plt.xlabel("Problem Size")
 plt.xscale('log')
 plt.ylabel("GiB/s")
-plt.title("MAX 1100 vs. V100 Local Memory Bandwidth")
+plt.title(f"{name1} vs. {name2} Local Memory Bandwidth")
 
 # Aggiungi una legenda
 plt.legend()
